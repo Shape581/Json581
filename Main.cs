250dc2d534a9ff2918265b42a7b2d8583d022813
Shape581 <severusrogue5008@gmail.com>
2025-01-10 23:43:12 +0100

    Code

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Life;
+
+namespace Json581
+{
+    public class Main : Plugin
+    {
+        public Main(IGameAPI api) : base(api) { }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("[Json581] - Succès : Initialise !");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
